dde1794ab02561ba85d073be7ab15602a807dbfd
Michael Kielstra <4313413+PMKielstra@users.noreply.github.com>
2020-08-26 20:32:33 +0100

    RegexAnalyzer searches everywhere, not just at start

--- civet/builtin_analyzers.py
+++ civet/builtin_analyzers.py
@@ -9,15 +9,17 @@ class RegexAnalyzer(Analyzer):
     Attributes:
         regex: The regex (Python flavour, obviously) to search for.  Can be either a re.Pattern or a string.
         search_in_err: Search in STDERR result instead of STDOUT.  False by default.
+        match_at_start_only: Call re.match instead of re.search, matching only at the beginning of the output or error text.  False by default.
     """
 
-    def __init__(self, regex, search_in_err=False):
+    def __init__(self, regex, search_in_err=False, match_at_start_only=False):
         """Initializes the RegexAnalyzer and compiles the passed-in regex if it hasn't been compiled already."""
         if isinstance(regex, re.Pattern):
-            self.regex = regex
+            compiledregex = regex
         else:
-            self.regex = re.compile(regex)
+            compiledregex = re.compile(regex)
         self.search_in_err = search_in_err
+        self.search = compiledregex.match if match_at_start_only else compiledregex.search
 
     def analyze(self, out, err):
         """Calls regex.match on STDOUT or STDERR, depending on configuration.
@@ -25,8 +27,7 @@ class RegexAnalyzer(Analyzer):
         Returns:
             The groupdict that results from the match, or {} if no match was found.
         """
-        match = self.regex.match(
-            err) if self.search_in_err else self.regex.match(out)
-        if not match:
-            return {}
-        return match.groupdict()
+        match = self.search(err) if self.search_in_err else self.search(out)
+        if match:
+            return match.groupdict()
+        return {}
